9466f4335cd9df958d0745d9729d1c835c3ede4d
up8954 <1099085515@qq.com>
2024-11-24 23:54:58 +0800

    Update screenInfo.js

--- screenInfo.js
+++ screenInfo.js
@@ -10,16 +10,12 @@ const screenData = {
     updateScreenInfo: function() {
       this.screenWidth = screen.width;  // 获取屏幕的实际宽度
       this.screenHeight = screen.height;  // 获取屏幕的实际高度
-      this.colorDepth = screen.colorDepth;  // 获取屏幕色盘深度
-      this.availWidth = screen.availWidth;  // 获取显示区域的宽度
-      this.availHeight = screen.availHeight;  // 获取显示区域的高度
       this.windowWidth = window.innerWidth;  // 获取当前浏览器窗口的宽度
       this.windowHeight = window.innerHeight;  // 获取当前浏览器窗口的高度
     },
     formatScreenInfo: function() {
       // 格式化屏幕信息为一个字符串，增加窗口大小信息
-      return `屏幕实际宽度: ${this.screenWidth}px, 屏幕实际高度: ${this.screenHeight}px, 色盘深度: ${this.colorDepth}-bit, ` +
-             `显示区域宽度: ${this.availWidth}px, 显示区域高度: ${this.availHeight}px, ` +
+     return `屏幕实际宽度: ${this.screenWidth}px, 屏幕实际高度: ${this.screenHeight}px,  ` +
              `浏览器窗口宽度: ${this.windowWidth}px, 浏览器窗口高度: ${this.windowHeight}px`;
     }
   };
